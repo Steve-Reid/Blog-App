eb25daa55bd377676193ff409cf493ab709cc359
Steve Reid <steve@appby.design>
2018-03-15 14:55:02 +0000

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI:
+    'mongodb://stevebydesign:gotkotkev@ds113799.mlab.com:13799/blog_dev',
+  cookieKey: '123123123'
+};
